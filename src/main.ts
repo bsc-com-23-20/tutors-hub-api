573eddde16e411fa468be75d37f7250b4c9cef71
bsc-com-23-20 <bsc-com-23-20@unima.ac.mw>
2023-05-09 07:53:18 +0100

    updated message in swagger document builder

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('TutorsHub API')
+    .setDescription('The tutors API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('tutor')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
