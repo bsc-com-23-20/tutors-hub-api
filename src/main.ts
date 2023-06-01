c5784dd880cea7a5daca4bbecf7ff0fc9f007926
bsc-com-23-20 <bsc-com-23-20@unima.ac.mw>
2023-06-01 12:31:52 +0100

    added a few commments

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // documenting swagger
   const config = new DocumentBuilder()
     .setTitle('TutorsHub API')
     .setDescription('Nestjs tutors Rest API ')
@@ -14,6 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // app listening at port 3001
   await app.listen(3001);
 }
 bootstrap();
