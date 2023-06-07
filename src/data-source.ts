9a778d6c4f308a5e833747a560267123c9d71816
bsc-com-23-20 <bsc-com-23-20@unima.ac.mw>
2023-06-07 09:17:08 +0100

    hosted the database online

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,11 +5,11 @@ import  {TypeOrmModuleOptions} from '@nestjs/typeorm'
 
 export const tutorshubDataSource: TypeOrmModuleOptions = {
     type: "mysql",
-    host: "localhost",
+    host: "sql7.freesqldatabase.com",
     port: 3306,
-    username: "root",
-    password: "mernana@03",
-    database: "tutorshub",
+    username: "sql7624240",
+    password: "nyNhEQhy8U",
+    database: "sql7624240",
     synchronize: true,
     logging: false,
     entities: [Tutor, Reviews],
